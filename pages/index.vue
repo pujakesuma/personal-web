3bee4b005a8a6332f84a59ff1dc5b766ccc2a3e6
ahmad ridwan <ahmadridwan.3107@gmail.com>
2021-01-31 16:48:53 +0700

    first

---
+++ pages/index.vue
@@ -0,0 +1,274 @@
+<template>
+  <!-- This example requires Tailwind CSS v2.0+ -->
+  <div class="relative bg-white overflow-hidden">
+    <div class="max-w-7xl mx-auto">
+      <div
+        class="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32"
+      >
+        <svg
+          class="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+          fill="currentColor"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+          aria-hidden="true"
+        >
+          <polygon points="50,0 100,0 50,100 0,100" />
+        </svg>
+
+        <div class="relative pt-6 px-4 sm:px-6 lg:px-8">
+          <nav
+            class="relative flex items-center justify-between sm:h-10 lg:justify-start"
+            aria-label="Global"
+          >
+            <div
+              class="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0"
+            >
+              <div class="flex items-center justify-between w-full md:w-auto">
+                <a href="#">
+                  <span class="sr-only">Workflow</span>
+                  <img class="h-8 w-auto sm:h-16" src="/logo.png" />
+                </a>
+                <div class="-mr-2 flex items-center md:hidden">
+                  <button
+                    type="button"
+                    class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                    id="main-menu"
+                    aria-haspopup="true"
+                    @click="isOpen = !isOpen"
+                  >
+                    <span class="sr-only">Open main menu</span>
+                    <!-- Heroicon name: menu -->
+                    <svg
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div class="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900"
+                >About</a
+              >
+
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900"
+                >Experiences</a
+              >
+
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900"
+                >Work</a
+              >
+
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900"
+                >Blog</a
+              >
+
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900"
+                >Contact</a
+              >
+
+              <!-- <a
+                href="#"
+                class="font-medium text-indigo-600 hover:text-indigo-500"
+                >Log in</a
+              > -->
+            </div>
+          </nav>
+        </div>
+
+        <!--
+        Mobile menu, show/hide based on menu open state.
+
+        Entering: "duration-150 ease-out"
+          From: "opacity-0 scale-95"
+          To: "opacity-100 scale-100"
+        Leaving: "duration-100 ease-in"
+          From: "opacity-100 scale-100"
+          To: "opacity-0 scale-95"
+      -->
+        <transition
+          enter-active-class="transition ease-out duration-100 transform"
+          enter-from-class="opacity-0 scale-95"
+          enter-to-class="opacity-100 scale-100"
+          leave-active-class="transition duration-100 ease-in transform"
+          leave-from-class="opacity-100 scale-100"
+          leave-to-class="opacity-0 scale-95"
+        >
+          <div
+            v-if="isOpen"
+            class="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+          >
+            <div
+              class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
+            >
+              <div class="px-5 pt-4 flex items-center justify-between">
+                <div>
+                  <img
+                    class="h-8 w-auto"
+                    src="/logo.png"
+                    alt="logo"
+                  />
+                </div>
+                <div class="-mr-2">
+                  <button
+                    type="button"
+                    @click="isOpen = !isOpen"
+                    class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+                  >
+                    <span class="sr-only">Close main menu</span>
+                    <!-- Heroicon name: x -->
+                    <svg
+                      class="h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+              <div
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="main-menu"
+              >
+                <div class="px-2 pt-2 pb-3 space-y-1" role="none">
+                  <a
+                    href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                    >About</a
+                  >
+
+                  <a
+                    href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                    >Experiences</a
+                  >
+
+                  <a
+                    href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                    >Work</a
+                  >
+
+                  <a
+                    href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                    >Blog</a
+                  >
+
+                  <a
+                    href="#"
+                    class="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                    role="menuitem"
+                    >Contact</a
+                  >
+                </div>
+                <!-- <div role="none">
+                  <a
+                    href="#"
+                    class="block w-full px-5 py-3 text-center font-medium text-indigo-600 bg-gray-50 hover:bg-gray-100"
+                    role="menuitem"
+                  >
+                    Log in
+                  </a>
+                </div> -->
+              </div>
+            </div>
+          </div>
+        </transition>
+
+        <main
+          class="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28"
+        >
+          <div class="sm:text-center lg:text-left">
+            <p
+              class="text-lg sm:text-xl md:text-2xl text-gray-500 mb-2 load-hidden v-scroll-reveal"
+            >
+              Hello there, my name is
+            </p>
+            <h1
+              class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl"
+            >
+              <span class="block text-red-900 xl:inline">Ahmad Ridwan</span>
+              <!-- <span class="block text-indigo-600 xl:inline"
+                >I Build</span
+              > -->
+            </h1>
+            <h2 class="text-2xl tracking-tight font-extrabold md:text-4xl text-gray-700">
+              Welcome to my Personal Web
+            </h2>
+            <p
+              class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
+              dolorem libero ducimus ex provident sequi, sit autem, itaque
+              perspiciatis quasi ipsum laudantium dolor consequatur facilis et,
+              vero atque! Perferendis, dolore!
+            </p>
+            <div
+              class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"
+            >
+              <div class="rounded-md shadow">
+                <a
+                  href="#"
+                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-900 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                >
+                  More about me
+                </a>
+              </div>
+              <div class="mt-3 sm:mt-0 sm:ml-3">
+                <a
+                  href="#"
+                  class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-red-900 bg-gray-200 hover:bg-gray-300 md:py-4 md:text-lg md:px-10"
+                >
+                  Get in touch
+                </a>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+    <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+      <img
+        class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+        src="/ar.png"
+        alt=""
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    isOpen: false,
+  }),
+};
+</script>
+
+<style></style>
